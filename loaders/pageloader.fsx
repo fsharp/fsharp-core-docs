1119a79208d52f148f1beb823cee5ba37ee84b93
Chet Husk <chusk3@gmail.com>
2020-06-01 09:55:53 -0500

    fix home link

--- loaders/pageloader.fsx
+++ loaders/pageloader.fsx
@@ -7,7 +7,7 @@ type Shortcut = {
 }
 
 let loader (projectRoot: string) (siteContent: SiteContents) =
-    siteContent.Add({title = "Home"; link = "/"; icon = "fas fa-home"})
+    siteContent.Add({title = "Home"; link = "/fsharp-core-api-docs"; icon = "fas fa-home"})
     siteContent.Add({title = "F# Software Foundation"; link = "http://fsharp.org"; icon = "fas fa-cubes"})
     siteContent.Add({title = "GitHub repo"; link = "https://github.com/dotnet/fsharp"; icon = "fab fa-github"})
     siteContent
