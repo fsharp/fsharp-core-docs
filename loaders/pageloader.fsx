9a061da1ee56f94a3b70378a3bb564bb56b29ff2
cartermp <pcarter@fastmail.com>
2020-07-15 17:07:10 -0700

    General updates:

--- loaders/pageloader.fsx
+++ loaders/pageloader.fsx
@@ -9,5 +9,5 @@ type Shortcut = {
 let loader (projectRoot: string) (siteContent: SiteContents) =
     siteContent.Add({title = "Home"; link = "/fsharp-core-api-docs"; icon = "fas fa-home"})
     siteContent.Add({title = "F# Software Foundation"; link = "http://fsharp.org"; icon = "fas fa-cubes"})
-    siteContent.Add({title = "GitHub repo"; link = "https://github.com/dotnet/fsharp"; icon = "fab fa-github"})
+    siteContent.Add({title = "F# GitHub repo"; link = "https://github.com/dotnet/fsharp"; icon = "fab fa-github"})
     siteContent
