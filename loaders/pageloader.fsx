59794b64c180340a9ec8b2b8cb28f77f72b0baf9
Chet Husk <chusk3@gmail.com>
2020-07-20 10:47:48 -0500

    add link to this docs repo

--- loaders/pageloader.fsx
+++ loaders/pageloader.fsx
@@ -9,5 +9,6 @@ type Shortcut = {
 let loader (projectRoot: string) (siteContent: SiteContents) =
     siteContent.Add({title = "Home"; link = "/fsharp-core-api-docs"; icon = "fas fa-home"})
     siteContent.Add({title = "F# Software Foundation"; link = "http://fsharp.org"; icon = "fas fa-cubes"})
-    siteContent.Add({title = "F# GitHub repo"; link = "https://github.com/dotnet/fsharp"; icon = "fab fa-github"})
+    siteContent.Add({title = "FSharp.Core source code"; link = "https://github.com/dotnet/fsharp"; icon = "fab fa-github"})
+    siteContent.Add({title = "These docs"; link = "https://github.com/fsharp/fsharp-core-api-docs"; icon = "fab fa-github"})
     siteContent
